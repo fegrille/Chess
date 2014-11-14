8bc44e143ef798326f1c93c2407693854aa0dfb0
fegrille <felixgrille@web.de>
2014-11-14 12:32:23 +0100

    Added javadoc to bishop class

--- chess/src/model/Bishop.java
+++ chess/src/model/Bishop.java
@@ -3,16 +3,28 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-//Läufer
+/**
+ * This class contains everything that is special for the bishop figure
+ * @author fegrille
+ */
 public class Bishop extends Figure{
 	
 	private Moves move = new Moves();
 	
+	/**
+	 * Constructor to initialize bishop
+	 * @param y - y-cords of the figure
+	 * @param x - x-cords of the figure
+	 * @param color - color of the player
+	 */
 	public Bishop(int y, int x, char color) {
 		setField(y,x);
 		setColor(color);
 	}
 	
+	/**
+	 * Method to get the fields where the bishop figure can go to
+	 */
 	@Override
 	public List<Integer[]> possibleFields() {
 		List<Integer[]> fields = new ArrayList<Integer[]>();
