0efdd3aeccbf14b5c4f3131b4f4e3c01278ba519
manuel <manuel@manuel-PC>
2014-12-04 13:33:12 +0100

    Finished Class ControlChess

--- chess/src/control/ControlChess.java
+++ chess/src/control/ControlChess.java
@@ -1,5 +1,49 @@
 package control;
 
-public class ControlChess {
+import model.Figure;
+import model.Player;
 
+public class ControlChess {
+	
+	boolean chess = false;
+	int[] kingField = {};
+
+	public boolean checkChess(Player p, Figure lastMovedFigure, ControlColidate col, Player p2) {
+		getKingField(p);
+		checkFigures(lastMovedFigure, col, p, p2);
+		return isChess();
+	}
+
+	private void getKingField(Player p) {
+		Figure king = p.getFigureList().get(p.getFigureList().size() - 1);
+		setKingField(king.getField());
+	}
+
+	private void checkFigures(Figure lastMovedFigure, ControlColidate col, Player p, Player p2) {
+		for(Integer[] field : col.colidate(lastMovedFigure, lastMovedFigure.possibleFields(), p2, p)) {
+			compareField(field);
+		}
+	}
+	
+	private void compareField(Integer[] field) {
+		if(getKingField()[0] == field[0] && getKingField()[1] ==  field[1]) {
+			setChess(true);
+		}
+	}
+
+	private int[] getKingField() {
+		return kingField;
+	}
+
+	private void setKingField(int[] kingField) {
+		this.kingField = kingField;
+	}
+
+	public boolean isChess() {
+		return chess;
+	}
+
+	public void setChess(boolean chess) {
+		this.chess = chess;
+	}
 }
--- chess/src/control/ControlEndGame.java
+++ chess/src/control/ControlEndGame.java
@@ -24,8 +24,10 @@ public class ControlEndGame {
 	private boolean cantKillBadFigure = false;
 	private boolean cantBlockBadFigure = false;
 	
-	public void controlWin(Player pl) {
-		if(checkChess(pl)) {
+	private ControlChess cChess = new ControlChess();
+	
+	public void controlWin(Player pl, Player plOpp,  ControlColidate col, Figure lastMovedFigure) {
+		if(cChess.checkChess(pl, lastMovedFigure, col, plOpp)) {
 			checkRetireOptions(pl);
 		}
 	}
@@ -37,13 +39,6 @@ public class ControlEndGame {
 		if(isKingCantMove() && isCantKillBadFigure() && isCantBlockBadFigure()) {
 			setWin(true);
 		}
-	}
-
-	private boolean checkChess(Player pl) {
-		/*
-		 * checking if chess
-		 */
-		return false;
 	}
 
 	private boolean blockBadFigure(Player pl) {
@@ -59,9 +54,9 @@ public class ControlEndGame {
 	private void checkPossibleFieldsKing(Player pl) {
 		int size = pl.getFigureList().size();
 		Figure king = pl.getFigureList().get(size - 1);
-		//if(king.possibleFields().size() == 0)
+		if(king.possibleFields().size() == 0)
 		setKingCantMove(true);
-		//
+		
 	}
 
 
