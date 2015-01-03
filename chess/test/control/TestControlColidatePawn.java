95b75f7e0382298bdd41b8a38466c8249cb9b488
manuel <manuel@manuel-PC>
2015-01-03 15:00:20 +0100

    Renamed a class

--- chess/test/control/TestControlColidatePawn.java
+++ chess/test/control/TestControlColidatePawn.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestControlColidatePawnKnight {
+public class TestControlColidatePawn {
 
 	@Before
 	public void setUp() throws Exception {
